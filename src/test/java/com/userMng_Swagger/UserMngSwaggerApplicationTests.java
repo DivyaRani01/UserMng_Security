2b83ae0e065b3c2839b01ca6c3417c0a74e40e03
DivyaRani01 <divya04200@gmail.com>
2022-11-03 22:18:45 +0530

    boot Swgger+security

---
+++ src/test/java/com/userMng_Swagger/UserMngSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userMng_Swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMngSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
